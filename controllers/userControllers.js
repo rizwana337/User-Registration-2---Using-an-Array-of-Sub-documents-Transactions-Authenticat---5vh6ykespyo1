feea1be6a3e53c34ab5cdc5489d084ef89c83507
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-05-07 19:52:37 +0530

    Update userControllers.js

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,29 +1,29 @@
 const users   =require("../models/user.js");
 
+const registerUser =async (req, res) => {
 
-/*
-Post request json file structure
+    var name = req.body.name;
+    var email  = req.body.email;
+    var password = req.body.password;
+    var DOB = req.body.DOB;
 
-
-    obj =  {
+    var newuser = {
         "name":name,
         "email":email,
         "password": password,
-        "DOB": DOB
-    }
+        "DOB": DOB,
+    };
 
- */
+    // console.log(newuser);
 
-//You need to complete the route of user register
-//you need to register the user and return the id assign to the user.
-//you will get error if user mail allready exist in that case you need to return 404 status with err message that you get.
-//to look the user schema look ../models/user.js
-
-
-const registerUser =async (req, res) => {
-
-    //write your code here
+    users.create(newuser).then((user) => {
+        res.send(user._id);
+    })
+    .catch((error) => {
+        res.status(404).send(error.message);
+    });
 
 }
 
 module.exports = { registerUser };
+Footer
